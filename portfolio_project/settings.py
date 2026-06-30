1c460a140fdb8fc4384d34ab38b0895a205a4305
Modi Ruchin <modiruchin@gmail.com>
2026-06-30 13:55:07 +0530

    Remove cache files and update settings

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xv^9$8^&p3emjvz8qbb)_k6w@i55f%hv_tw_*6n)46$0h5v()_'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
